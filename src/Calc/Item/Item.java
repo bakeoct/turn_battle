a8967f80da9d2d64fc97084932861b6ee5ee0bfe
bake oct <2009takumi@gmail.com>
2023-04-23 17:10:53 +0900

    20230423

--- src/Calc/Item/Item.java
+++ src/Calc/Item/Item.java
@@ -3,10 +3,6 @@ package Calc.Item;
 import java.util.ArrayList;
 
 public abstract class Item {
-    public ArrayList <String> item =new ArrayList<String>();
     public Boolean have = false;
-    public Boolean cliff = false;
-    public Boolean Ocean = false;
-    public Boolean mounten =false;
     public String name;
 }
